f6baa5152dcfafca7c20c4125d5e2bad6aff2d48
Siarhei Prymakovich <prima.psa@gmail.com>
2022-04-30 20:36:48 +0300

    feat: implement stack

--- src/stack.js
+++ src/stack.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * Implement the Stack with a given interface via array.
@@ -13,22 +13,22 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  */
 class Stack {
-  push(/* element */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor() {
+    this._container = [];
+  }
+  push(element) {
+    this._container[this._container.length] = element;
   }
 
   pop() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this._container.splice(-1)[0];
   }
 
   peek() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this._container[this._container.length - 1];
   }
 }
 
 module.exports = {
-  Stack
+  Stack,
 };
